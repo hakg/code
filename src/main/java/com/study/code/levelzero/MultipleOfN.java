b80f35479e2a70e5adc9c9aa787f61af0916a869
hakg <tisccool@gmail.com>
2023-08-01 01:28:24 +0900

    n의 배수 결과 구하기

---
+++ src/main/java/com/study/code/levelzero/MultipleOfN.java
@@ -0,0 +1,18 @@
+package com.study.code.levelzero;
+
+public class MultipleOfN {
+    /**
+     * n의 배수
+     *
+     * 문제 설명
+     * 정수 num과 n이 매개 변수로 주어질 때, num이 n의 배수이면 1을 return n의 배수가 아니라면 0을 return하도록 solution 함수를 완성해주세요.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        int num = 11;
+        int n = 5;
+
+        System.out.println((num % n == 0 ? 1 : 0));
+    }
+}
